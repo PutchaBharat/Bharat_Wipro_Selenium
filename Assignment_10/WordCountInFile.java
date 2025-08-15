8ade255235f262df670f43e4d64cd8a619fd77c3
PutchaBharat <pucha.bharath@gmail.com>
2025-08-15 14:15:50 +0530

    Add files via upload

---
+++ Assignment_10/WordCountInFile.java
@@ -0,0 +1,34 @@
+package Assignment_10;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class WordCountInFile {
+    public static void main(String[] args) {
+        
+        String filename = "student.txt";
+        String wordToFind = "Bharat";
+
+        int count = 0;
+
+        try (Scanner fileScanner = new Scanner(new File(filename))) 
+        {
+           
+            while (fileScanner.hasNext()) {
+                String word = fileScanner.next();
+                word = word.replaceAll("\\W", "");
+                if (word.equalsIgnoreCase(wordToFind)) 
+                {
+                    count++;
+                }
+            }
+            System.out.println("The word '" + wordToFind + "' occurred " + count + " times in the file.");
+        } 
+        catch (FileNotFoundException e) 
+        {
+            System.out.println("File not found: " + filename);
+        }
+    }
+}
